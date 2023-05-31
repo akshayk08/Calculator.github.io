ee57a5c82a76c937e436feaac453fdf83c100fd1
akshayk08 <115618817+akshayk08@users.noreply.github.com>
2023-05-31 09:44:22 +0530

    added index.html

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function add(){
+    let input1 = document.querySelector("#id1").value;
+    let input2 = document.querySelector("#id2").value;
+  
+    let add = parseInt(input1) + parseInt(input2);
+  
+    alert(add);
+  }
+  function subtract(){
+    let input1 = document.querySelector("#id1").value;
+    let input2 = document.querySelector("#id2").value;
+  
+    let sub = parseInt(input1) - parseInt(input2);
+  
+    alert(sub);
+  }
+  function multiply(){
+    let input1 = document.querySelector("#id1").value;
+    let input2 = document.querySelector("#id2").value;
+  
+    let mul = parseInt(input1) * parseInt(input2);
+  
+    alert(mul);
+  }
+  function divide(){
+    let input1 = document.querySelector("#id1").value;
+    let input2 = document.querySelector("#id2").value;
+  
+    let div = parseInt(input1) / parseInt(input2);
+  
+     alert(div);}
